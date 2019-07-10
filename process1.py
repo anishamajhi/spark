0d946d941e7bca68e5260dbadc8fdd90cc585fa0
anishamajhi <anishamajhi085@gmail.com>
2019-07-10 12:34:21 +0530

    Update process1.py

--- process1.py
+++ process1.py
@@ -1,9 +1,4 @@
-print("Hello")
-#from pyspark.sql import SparkSession
-#import subprocess
-#spark = SparkSession.builder.master("k8s://https://127.0.0.1:6443").appName("PythonSparkStreamingKafka_RM_01").getOrCreate()
-#spark.conf.set('spark.executor.cores', '3')
-#print(spark)
-#subprocess.Popen(["kubectl","get","pods"],shell=True, stdout=subprocess.PIPE)
-while(1==1):
-   print("Hello loop")
+class test:
+        print("Hello")
+        while(1==1):
+                print("Hello loop")
